fa313d1d334fdc0ad637a49c080a340e2a25f366
Shobhit Keshri <shobhitkes512@gmail.com>
2026-04-21 10:59:04 +0530

    fix: enforce strict eth_type matching to prevent ARP bypass of QoS queues

--- qos_controller.py
+++ qos_controller.py
@@ -3,11 +3,9 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
+from ryu.lib.packet import packet, ethernet, ether_types, ipv4, in_proto
 
 class SimpleQoSController(app_manager.RyuApp):
-    # THE FIX IS ON THIS LINE BELOW:
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
@@ -40,23 +38,46 @@ class SimpleQoSController(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            return
+
         dst = eth.dst
         src = eth.src
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
-
         self.mac_to_port[dpid][src] = in_port
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
+        out_port = self.mac_to_port[dpid][dst] if dst in self.mac_to_port[dpid] else ofproto.OFPP_FLOOD
 
-        actions = [parser.OFPActionOutput(out_port)]
+        # ==========================================
+        # QoS LOGIC: Strict Traffic Identification
+        # ==========================================
+        queue_id = None
+        
+        # THE FIX: We explicitly include eth_type so ARP doesn't blanket-cover IP traffic
+        match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_type=eth.ethertype)
+
+        if eth.ethertype == ether_types.ETH_TYPE_IP:
+            ip = pkt.get_protocol(ipv4.ipv4)
+            if ip.proto == in_proto.IPPROTO_ICMP:
+                queue_id = 1  # VIP Lane
+                self.logger.info("-> High Priority ICMP (Ping) Detected! VIP Queue 1.")
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_type=ether_types.ETH_TYPE_IP, ip_proto=ip.proto)
+            elif ip.proto == in_proto.IPPROTO_UDP:
+                queue_id = 0  # Slow Lane
+                self.logger.info("-> Low Priority UDP (Junk) Detected! Slow Queue 0.")
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_type=ether_types.ETH_TYPE_IP, ip_proto=ip.proto)
+
+        actions = []
+        if queue_id is not None:
+            actions.append(parser.OFPActionSetQueue(queue_id))
+        actions.append(parser.OFPActionOutput(out_port))
+
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            self.add_flow(datapath, 1, match, actions)
+            self.add_flow(datapath, 10, match, actions)
 
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None)
+                                  in_port=in_port, actions=actions, 
+                                  data=msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None)
         datapath.send_msg(out)
