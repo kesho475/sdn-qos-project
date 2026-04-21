bc23eccacde0d12e22dfb87f1b10a285b733b9ae
Shobhit Keshri <shobhitkes512@gmail.com>
2026-04-21 10:52:40 +0530

    feat: implemented QoS logic with OpenFlow queues for ICMP prioritization

--- topology.py
+++ topology.py
@@ -3,6 +3,7 @@ from mininet.net import Mininet
 from mininet.node import RemoteController, OVSSwitch
 from mininet.link import TCLink
 from mininet.cli import CLI
+import os
 
 class QoSTopo(Topo):
     def build(self):
@@ -16,6 +17,13 @@ def run():
     topo = QoSTopo()
     net = Mininet(topo=topo, controller=RemoteController, link=TCLink)
     net.start()
+    
+    print("\n*** Configuring Hardware QoS Queues on Switch 1 ***")
+    # Queue 0 (Slow Lane): Max 2 Mbps
+    # Queue 1 (VIP Lane): Min 8 Mbps, Max 10 Mbps
+    os.system("ovs-vsctl -- set Port s1-eth1 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=10000000 queues=0=@q0,1=@q1 -- --id=@q0 create Queue other-config:max-rate=2000000 -- --id=@q1 create Queue other-config:min-rate=8000000 other-config:max-rate=10000000 > /dev/null 2>&1")
+    os.system("ovs-vsctl -- set Port s1-eth2 qos=@newqos2 -- --id=@newqos2 create QoS type=linux-htb other-config:max-rate=10000000 queues=0=@q0,1=@q1 -- --id=@q0 create Queue other-config:max-rate=2000000 -- --id=@q1 create Queue other-config:min-rate=8000000 other-config:max-rate=10000000 > /dev/null 2>&1")
+    
     CLI(net)
     net.stop()
 
